d62563e847f6ef9a5562bbb772e6d5bd01b74c2b
Andrii Paziuk <andrii.paziuk@gmail.com>
2022-07-04 23:22:05 +0200

    Updated 04.07.2022

---
+++ js/app.js
@@ -0,0 +1,14 @@
+VANTA.BIRDS({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x30e1e,
+    color1: 0xfff000,
+    color2: 0xa3ff,
+    colorMode: "lerp"
+  })
