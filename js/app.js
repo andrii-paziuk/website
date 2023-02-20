8354cdbe48bfc9a15386e607b1b2cf3e9d232091
Andrii Paziuk <andrii.paziuk@gmail.com>
2023-02-20 16:39:56 +0100

    Updating website

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 VANTA.BIRDS({
-  el: "body",
+  el: "html",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 800.00,
-  minWidth: 200.00,
+  minHeight: 500.00,
+  minWidth: 500.00,
   scale: 1.00,
   scaleMobile: 1.00,
   backgroundColor: 0x7192f,
